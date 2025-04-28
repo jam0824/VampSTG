de9542497cedd0ff992ac0517c407410cdfb3af5
jam0824 <session_fixation@hotmail.co.jp>
2025-04-28 22:51:28 +0900

    ステージのプログレスバーを付けた

---
+++ Script/UI/ProgressBar.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class ProgressBar : MonoBehaviour
+{
+    [Header("Progress Bar")]
+    [SerializeField] GameObject bar;
+    [SerializeField] float barAllLength = 3f;
+    float allTime = 3f * 60f;
+    private float elapsedTime = 0f;
+    [Header("Player Icon")]
+    [SerializeField] GameObject playerIcon;
+    [SerializeField] float wayAllLength = 12f;
+    [SerializeField] float defaultIconZ = -6f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        float per = elapsedTime / allTime;
+        DrawProgressBar(per, barAllLength);
+        DrawPlayerIcon(per, wayAllLength);
+        
+    }
+
+    public void DrawProgressBar(float perProgress, float spriteAllLength){
+        // プログレスバーの長さを変更
+        float perLeft = 1.0f - perProgress;
+        Vector3 scale = bar.transform.localScale;
+        scale.x = spriteAllLength * perLeft;
+        bar.transform.localScale = scale;
+
+        //プログレスバーの描画位置を調整
+        float offset = spriteAllLength * perProgress;
+        Vector3 pos = bar.transform.position;
+        pos.z = offset * 2;
+        bar.transform.position = pos;
+    }
+    public void DrawPlayerIcon(float perProgress, float wayAllLength){
+        float offset = perProgress * wayAllLength;
+        Vector3 pos = playerIcon.transform.position;
+        pos.z = defaultIconZ + offset;
+        playerIcon.transform.position = pos;
+    }
+}
