9c6cc1ccff709b7cccb062e4fab8bab09272609e
jam0824 <session_fixation@hotmail.co.jp>
2025-05-14 21:31:36 +0900

    BaseBossを使ってoverrideする形で作成

---
+++ Script/Boss/BaseBoss.cs
@@ -0,0 +1,158 @@
+// BaseBoss.cs
+using UnityEngine;
+using System.Collections;
+
+public abstract class BaseBoss : MonoBehaviour, IBoss
+{
+    [Header("ステータス")]
+    [SerializeField] protected float hp = 500f;
+    [SerializeField] protected float attackInterval = 2f;
+    [SerializeField] protected BossHpBar bossHpBar;
+    [SerializeField] protected ScreenFader fader;
+
+    [Header("ホーミングミサイルのターゲット名")]
+    [SerializeField] protected string missileTargetName = "MissileTarget";
+
+    protected float maxHp;
+    protected PlayerManager playerManager;
+    protected string tagName = "Boss";
+    protected bool isDead = false;
+    protected bool isStart = false;
+    protected Animator animator;
+
+    protected virtual void Start()
+    {
+        // 必要ならサブクラスで拡張
+    }
+
+    protected virtual void Update()
+    {
+        if (isDead) return;
+        DrawHpBar();
+    }
+
+    // IBoss 実装
+    public virtual void PlayEntry()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        playerManager = GameObject.FindGameObjectWithTag("Core")
+                              .GetComponent<PlayerManager>();
+
+        maxHp = hp;
+        SetTagName(tagName);
+
+        StopAllCoroutines();
+        StartCoroutine(EntryCoroutine());
+    }
+    /// <summary>
+    /// 共通のトリガー処理。PlayerBullet タグの弾だけを受け付け、
+    /// ダメージ計算→共通 SE/エフェクト→死亡処理まで行う。
+    /// </summary>
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (!isStart || isDead) return;
+        if (!other.CompareTag("PlayerBullet")) return;
+        if (!other.TryGetComponent<ConfigPlayerBullet>(out var bullet)) return;
+
+        HandleBulletHit(bullet, other);
+    }
+
+    /// <summary>
+    /// 弾がヒットしたときの共通処理。
+    /// 必要ならサブクラスでオーバーライドして前後に処理をはさめる。
+    /// </summary>
+    protected virtual void HandleBulletHit(ConfigPlayerBullet bullet, Collider other)
+    {
+        // HP 減少
+        hp -= bullet.getDamage();
+
+        // ヒット SE
+        if (bullet.hitSe != null)
+            SoundManager.Instance.PlaySE(bullet.hitSe, bullet.hitSeVolume);
+
+        // ヒットエフェクト
+        if (bullet.triggerEffect != null)
+            Instantiate(
+                bullet.triggerEffect,
+                other.transform.position,
+                other.transform.rotation
+            );
+
+        // 弾の破壊
+        if (bullet.isDestroy)
+            Destroy(other.gameObject);
+
+        // 死亡判定
+        if (hp <= 0)
+            Die(other.transform);
+    }
+
+    protected virtual IEnumerator EntryCoroutine()
+    {
+        // 例：エントリー演出
+        yield return new WaitForSeconds(5f);
+        bossHpBar.StartFadeIn(3f);
+        gameObject.SetActive(true);
+        SoundManager.Instance.PlayBGM(GetEntryBGM(), GetEntryBGMVolume());
+        yield return new WaitForSeconds(attackInterval);
+        isStart = true;
+    }
+
+    /// <summary>
+    /// 演出用のBGM。サブクラスで差し替えたい場合は override。
+    /// </summary>
+    protected virtual AudioClip GetEntryBGM() => null;
+    protected virtual float GetEntryBGMVolume() => 1f;
+
+    /// <summary>
+    /// ダメージによる死亡処理。サブクラスから呼ぶ or override 可。
+    /// </summary>
+    public virtual void Die(Transform hitPoint)
+    {
+        isDead = true;
+        // 共通：キルカウント＆スコア加算
+        AddKillCount();
+        AddScore(maxHp);
+        // 必要ならフェードアウトなど
+    }
+
+    /// <summary>
+    /// 子オブジェクトにタグをセット
+    /// </summary>
+    public virtual void SetTagName(string tag)
+    {
+        foreach (var t in GetComponentsInChildren<Transform>(true))
+        {
+            if (t.gameObject.name != missileTargetName)
+                t.gameObject.tag = tag;
+        }
+    }
+
+    /// <summary>
+    /// HPバーを更新
+    /// </summary>
+    public virtual void DrawHpBar()
+    {
+        float per = (maxHp - hp) / maxHp;
+        bossHpBar.DrawBar(per);
+    }
+
+    /// <summary>
+    /// キル数をインクリメント
+    /// </summary>
+    public virtual void AddKillCount()
+    {
+        GameManager.Instance.killCount++;
+        GameManager.Instance.allKillCount++;
+    }
+
+    /// <summary>
+    /// スコアに加算
+    /// </summary>
+    public virtual void AddScore(float maxHp)
+    {
+        GameManager.Instance.AddScore(maxHp);
+    }
+}
--- Script/Boss/BossSandWorm.cs
+++ Script/Boss/BossSandWorm.cs
@@ -1,19 +1,92 @@
+// BossSandWorm.cs
 using UnityEngine;
+using System.Collections;
 
-public class BossSandWorm : MonoBehaviour,IBoss
+public class BossSandWorm : BaseBoss
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("BGM設定")]
+    [SerializeField] private AudioClip bgm;
+    [SerializeField] private float bgmVol = 0.8f;
+
+    int attackPattern = -1;
+
+    /// <summary>
+    /// EntryCoroutine の BGM を上書き
+    /// </summary>
+    protected override AudioClip GetEntryBGM() => bgm;
+    protected override float GetEntryBGMVolume() => bgmVol;
+
+    protected override void Start()
     {
+        base.Start();
+        // 初期非アクティブ化など、固有初期化
+        gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-
+        base.Update();
+        SwitchAttackPattern();
     }
 
-    public void PlayEntry(){
+    void SwitchAttackPattern(){
+        float hpPer = hp / maxHp;
+        if(isStart){
+            if(attackPattern == -1){
+                attackPattern = 0;
+                StopAllCoroutines();
+                StartCoroutine(Attack0Coroutine());
+            }
+            else if((hpPer < 0.66f) && (attackPattern == 0)){
+                attackPattern = 1;
+                StopAllCoroutines();
+                StartCoroutine(Attack1Coroutine());
+            }
+            else if((hpPer < 0.33f) && (attackPattern == 1)){
+                attackPattern = 2;
+                StopAllCoroutines();
+                StartCoroutine(Attack2Coroutine());
+            }
+        }
+    }
 
+    protected override IEnumerator EntryCoroutine()
+    {
+        // 例：エントリー演出
+        yield return new WaitForSeconds(5f);
+        bossHpBar.StartFadeIn(3f);
+        gameObject.SetActive(true);
+        SoundManager.Instance.PlayBGM(GetEntryBGM(), GetEntryBGMVolume());
+        yield return new WaitForSeconds(attackInterval);
+        isStart = true;
+    }
+
+    IEnumerator Attack0Coroutine(){
+        while(true){
+            yield return new WaitForSeconds(attackInterval);
+        }
+    }
+
+    IEnumerator Attack1Coroutine(){
+        while(true){
+            yield return new WaitForSeconds(attackInterval);
+        }
+    }
+
+    IEnumerator Attack2Coroutine(){
+        while(true){
+            yield return new WaitForSeconds(attackInterval);
+        }
+    }
+
+
+    /// <summary>
+    /// 死亡時の固有演出があれば追加
+    /// </summary>
+    public override void Die(Transform hitPoint)
+    {
+        base.Die(hitPoint);
+        animator.SetTrigger("Die");
+        // 砂虫特有の爆発エフェクトなど
     }
 }
