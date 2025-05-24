7287e4bb45c58625e050ff9c6627cba2d1e9ec94
jam0824 <session_fixation@hotmail.co.jp>
2025-05-24 15:27:06 +0900

    キャラクター追加から解放まで一通り完成

---
+++ Script/CharacterSelect/CharacterSelectUnlock.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectUnlock : MonoBehaviour
+{
+    [Header("キャラクター名（アンロック時の名前と合わせること）")]
+    [SerializeField] private string characterName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UnlockCharacter(characterName);
+    }
+
+    void UnlockCharacter(string characterName){
+        if(GameManager.Instance.gotCharacters.Contains(characterName)){
+            gameObject.GetComponent<Button>().interactable = true;
+        }
+        else{
+            gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
+}
